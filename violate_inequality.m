4aa3a1d4d8e332f7d670c1f2f66618429c38419f
matthieuzone <matthieu.bruant@gmail.com>
2025-06-19 12:27:41 +0200

    .

--- violate_inequality.m
+++ violate_inequality.m
@@ -1,25 +1,12 @@
-function [nW, nM, s] = opt(W, M)
+ineq = [5 0 0 0 0 0 0 0 0 -1 0 0 0 0 0 0 0 -1 0 0 0 0 0 0 0 0 0 0 -1 0 0 0 0 -1 0 0 0 0 0 0 0 0 0 0 0 0 -1 0 0 0 0 0 0 0 0 -1 0 -1 0 0 0 0 0 0 0];
+ops = sdpsettings('solver','mosek','verbose',0);
 
-    ineq = [5 0 0 0 0 0 0 0 0 -1 0 0 0 0 0 0 0 -1 0 0 0 0 0 0 0 0 0 0 -1 0 0 0 0 -1 0 0 0 0 0 0 0 0 0 0 0 0 -1 0 0 0 0 0 0 0 0 -1 0 -1 0 0 0 0 0 0 0];
-    ops = sdpsettings('solver','mosek','verbose',0);
+d = [2, 2, 2, 2, 2, 2];
+parties = {{[]}, {1, 2}, {3, 4}, {5, 6}, {[]}};
 
-    table = reshape(ineq(2:end), 8, 8);
-    score = @(x) sum(x .* table, 'all');
+table = reshape(ineq(2:end), 8, 8);
 
-    d = [2, 2, 2, 2, 2, 2];
-    parties = {{[]}, {1, 2}, {3, 4}, {5, 6}, {[]}};
-
-    cst = is_2causal(W, d, parties);
-    cst = et(cst, nullconstraints(trace(W) - 1));
-    for p = 1:3
-        for x = 1:2
-            cst = et(cst, is_PSD(M{p}{x}{1}));
-            cst = et(cst, is_PSD(M{p}{x}{2}));
-            cst = et(cst, nullconstraints(PartialTrace(M{p}{x}{1} + M{p}{x}{2}, 1, [2 2]) - eye(2)));
-        end
-        cst = et(cst, is_PSD(M{p}{2}{1}));
-        cst = et(cst, nullconstraints(PartialTrace(M{p}{2}{1}, 1, [2 2]) - eye(2)));
-    end
+function score = score(W, M, table)
     proba = sdpvar(8,8,'full');
     for o = 0:7
         for i = 0:7
@@ -28,44 +15,35 @@ function [nW, nM, s] = opt(W, M)
             proba(i+1,o+1) = real(trace(W * Tensor(M{1}{bi(1)+1}{bo(1)+1}, M{2}{bi(2)+1}{bo(2)+1}, M{3}{bi(3)+1}{bo(3)+1})));
         end
     end
-    optimize(cst, score(proba), ops);
-    nW = recvalue(W);
-    nM = recvalue(M);
-    s = score(value(proba));
+    score = sum(proba .* table, 'all');
 end
 
-function v = recvalue(x)
-    if iscell(x)
-        v = cellfun(@recvalue, x, 'UniformOutput', false);
-    elseif isa(x, 'sdpvar')
-        v = value(x);
-    else
-        v = x;
-    end
-end
-
-M = {{rpair(), {rsim(), zeros(4,4)}}, 
-     {rpair(), {rsim(), zeros(4,4)}}, 
-     {rpair(), {rsim(), zeros(4,4)}}};
+M = {{rpair(), {eye(4)/2, zeros(4,4)}}, 
+     {rpair(), {eye(4)/2, zeros(4,4)}}, 
+     {rpair(), {eye(4)/2, zeros(4,4)}}};
 
 for iter = 1:10
     disp(['Iteration: ', num2str(iter)]);
 
     disp('Optimizing W');
     W = sdpvar(2^6, 2^6, 'hermitian', 'complex');
-    [W, M, s] = opt(W, M);
-    disp(['Score: ', num2str(s)]);
+    cst = is_2causal(W, d, parties);
+    cst = et(cst, nullconstraints(trace(W) - 1));
+
+    optimize(cst, score(W, M, table), ops);
+    W = value(W);
+    disp(['Score: ', num2str(score(W, M, table))]);
 
     for i = 1:3
         for a = 1:2
             disp(['Optimizing M{', num2str(i), '}{1}{', num2str(a), '}']);
             M{i}{1}{a} = sdpvar(4,4);
-            [W, M, s] = opt(W, M);
-            disp(['Score: ', num2str(s)]);
+            cst = is_PSD(M{i}{1}{a});
+            cst = et(cst, nullconstraints(PartialTrace(M{i}{1}{1} + M{i}{1}{2}, 1, [2 2]) - eye(2)));
+            
+            optimize(cst, score(W, M, table), ops);
+            M{i}{1}{a} = value(M{i}{1}{a});
+            disp(['Score: ', num2str(score(W, M, table))]);
         end
-        disp(['Optimizing M{', num2str(i), '}{2}{1}']);
-        M{i}{2}{1} = sdpvar(4,4);
-        [W, M, s] = opt(W, M);
-        disp(['Score: ', num2str(s)]);
     end
 end
