58de876acc57a5084ae65fdc01552ae39190dd2b
matthieuzone <matthieu.bruant@gmail.com>
2025-06-19 16:36:05 +0200

    .

--- ineqs/max_inequality.m
+++ ineqs/max_inequality.m
@@ -1,13 +1,19 @@
 function [W, M] = max_inequality(ineq, class, niter)
 
-    ops = sdpsettings('solver','mosek','verbose',1);
+    ops = sdpsettings('solver','mosek','verbose',0);
 
     d = [2, 2, 2, 2, 2, 2];
     p = {{[]}, {1, 2}, {3, 4}, {5, 6}, {[]}};
 
-    M = {{rpair(), {eye(4)/2, zeros(4,4)}}, 
-        {rpair(), {eye(4)/2, zeros(4,4)}}, 
-        {rpair(), {eye(4)/2, zeros(4,4)}}};
+    
+    idd = [1,0,0,1];
+    id_CJ = pure_to_mixed(idd);
+    p0 = pure_to_mixed([1,0]);
+    p1 = pure_to_mixed([0,1]);
+    t01 = Tensor(p0, p1);
+    M = {{{t01, t01}, {id_CJ, zeros(4,4)}}, 
+        {{t01, t01} , {id_CJ, zeros(4,4)}}, 
+        {{t01, t01}, {id_CJ, zeros(4,4)}}};
     % M = {{{eye(4)/2, zeros(4,4)}, {eye(4)/2, zeros(4,4)}}, 
     %     {{eye(4)/2, zeros(4,4)}, {eye(4)/2, zeros(4,4)}}, 
     %     {{eye(4)/2, zeros(4,4)}, {eye(4)/2, zeros(4,4)}}};
@@ -15,7 +21,7 @@ function [W, M] = max_inequality(ineq, class, niter)
     for iter = 1:niter
         disp(['Iteration: ', num2str(iter)]);
 
-        disp('Optimizing W');
+        %disp('Optimizing W');
         W = sdpvar(2^6, 2^6, 'hermitian', 'complex');
         switch class
             case 'causal'
@@ -25,7 +31,7 @@ function [W, M] = max_inequality(ineq, class, niter)
             case '2-causal'
                 cst = is_2causal(W, d, p);
             case 'some-causality'
-                cst = asmit_some_causality(W, d, p);
+                cst = admit_some_causality(W, d, p);
             case 'valid'
                 %cst = superop_in_valid_cone(W, d, p);
                 cst = is_valid(W, d, p);
@@ -36,10 +42,10 @@ function [W, M] = max_inequality(ineq, class, niter)
 
         optimize(cst, score(calc_proba(W, M), ineq), ops);
         W = value(W);
-        disp(['Score: ', num2str(score(calc_proba(W, M), ineq))]);
+        %disp(['Score: ', num2str(score(calc_proba(W, M), ineq))]);
 
         for i = 1:3
-            disp(['Optimizing M{', num2str(i), '}{1}']);
+            %disp(['Optimizing M{', num2str(i), '}{1}']);
             M{i}{1}{1} = sdpvar(4,4, 'hermitian', 'complex');
             M{i}{1}{2} = sdpvar(4,4, 'hermitian', 'complex');
             cst = et(is_PSD(M{i}{1}{2}), is_PSD(M{i}{1}{1}));
@@ -47,7 +53,15 @@ function [W, M] = max_inequality(ineq, class, niter)
             optimize(cst, score(calc_proba(W, M), ineq), ops);
             M{i}{1}{1} = value(M{i}{1}{1});
             M{i}{1}{2} = value(M{i}{1}{2});
-            disp(['Score: ', num2str(score(calc_proba(W, M), ineq))]);
+            %disp(['Score: ', num2str(score(calc_proba(W, M), ineq))]);
+
+            %disp(['Optimizing M{', num2str(i), '}{2}']);
+            M{i}{2}{1} = sdpvar(4,4, 'hermitian', 'complex');
+            cst = is_PSD(M{i}{2}{1});
+            cst = et(cst, nullconstraints(PartialTrace(M{i}{2}{1}, 2, [2 2]) - eye(2)));
+            optimize(cst, score(calc_proba(W, M), ineq), ops);
+            M{i}{2}{1} = value(M{i}{2}{1});
         end
+        disp(['Score: ', num2str(score(calc_proba(W, M), ineq))]);
     end
 end
--- tests.m
+++ tests.m
@@ -1,17 +1,10 @@
-proba = zeros(8, 8);
-for x = 1:8
-    proba(x, 1) = 1;
+ineqs = readmatrix('data/3lazy_2-causal_repr.txt');
+
+for i = 1:size(ineqs, 1)
+    ineq = ineqs(i, :);
+    disp(['Inequality ', num2str(i), ':']);
+    [W, M] = max_inequality(ineq, '2-causal', 3);
+    s = score(calc_proba(W, M), ineq);
+    disp(['Violated: ']);
+    disp(s < -ineq(1) - 1e-3);
 end
-ineq = readmatrix('interesting.txt');
-ineq = ineq(1,:);
-score(proba, ineq)
-
-W = eye(8*8)/8;
-M = {{{eye(4)/2, zeros(4,4)}, {eye(4)/2, zeros(4,4)}}, 
-    {{eye(4)/2, zeros(4,4)}, {eye(4)/2, zeros(4,4)}}, 
-    {{eye(4)/2, zeros(4,4)}, {eye(4)/2, zeros(4,4)}}};
-
-probabis = calc_proba(W, M);
-disp(probabis);
-disp('Score:');
-disp(score(probabis, ineq));
